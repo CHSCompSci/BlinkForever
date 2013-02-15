f3b151a8441ae12aa61d5dbf2cb50a971676dc04
Avijeet Sachdev <info@avijeets.com>
2013-02-15 17:24:37 -0500

    comments

--- BlinkForever.ino
+++ BlinkForever.ino
@@ -1,23 +1,20 @@
 /*
-  Blink
+  BlinkForever
   Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
+  
+  The LED light stops flashing after it's flashed 100 times. 
  */
  
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+// Pin 13 has an LED connected on most Arduino boards
 int led = 13;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
+void setup() {
   pinMode(led, OUTPUT);     
 }
 
-// the loop routine runs over and over again:
+// the loop routine runs over and over again
 void loop() {
-  for (int i = 1; i < 00; i++) {
+  for (int i = 1; i < 00; i++) { // Change "i < 100" for the number you want
     digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);               // wait for a second
     digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
